71a5ba2eb01dc9d6530db02560fe74d73f6bcf5d
Xinghan Wang <xinghanw@usc.edu>
2016-01-09 10:50:56 -0800

    Add audio and custom fonts

--- PokeDex-app/ViewController.swift
+++ PokeDex-app/ViewController.swift
@@ -7,17 +7,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     @IBOutlet weak var collection: UICollectionView!
     var pokemon = [Pokemon]()
+    var musicPlayer: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         collection.delegate = self
         collection.dataSource = self
         parsePokemonCSV()
+        initAudio()
+    }
+    
+    func initAudio(){
+        let path = NSBundle.mainBundle().pathForResource("music", ofType: "mp3")!
+        
+        do{
+                musicPlayer = try AVAudioPlayer(contentsOfURL: NSURL(string:path)!)
+                musicPlayer.prepareToPlay()
+                musicPlayer.numberOfLoops = -1
+                musicPlayer.play()
+        }catch let err as NSError{
+            print(err.debugDescription)
+        }
+        
     }
     
     func parsePokemonCSV(){
@@ -64,5 +81,15 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         return CGSizeMake(105, 105)
     }
 
+    @IBAction func audioButtonPressed(sender: UIButton!) {
+        if musicPlayer.playing{
+            musicPlayer.stop()
+            sender.alpha = 0.2
+        }
+        else{
+            musicPlayer.play()
+            sender.alpha = 1.0
+        }
+    }
 }
 
