486235f39b6fdfae64865b0e924a18a5c8a1e1bc
Xinghan Wang <xinghanw@usc.edu>
2016-01-09 15:51:39 -0800

    Modify pokemon class

--- PokeDex-app/PokemonDetailVC.swift
+++ PokeDex-app/PokemonDetailVC.swift
@@ -10,7 +10,21 @@ import UIKit
 
 class PokemonDetailVC: UIViewController {
     
+    @IBAction func backLblPressed(sender: UIButton) {
+       dismissViewControllerAnimated(true, completion: nil)
+    }
     @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
     
     var pokemon:Pokemon!
 
