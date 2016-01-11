dd154a82febfa3af458964eb94ba35f365aa20e6
Xinghan Wang <xinghanw@usc.edu>
2016-01-10 16:37:39 -0800

    Retrieve and parse JSON pokemon data using alamofire

---
+++ PokeDex-app/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  PokeDex-app
+//
+//  Created by Xinghan Wang on 1/9/16.
+//  Copyright © 2016 Xinghan Wang. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co/"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
